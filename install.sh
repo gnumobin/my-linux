ad1011f40b6ced71487520ba5b98430a5311510c
gnumobin <gnumobin@gmail.com>
2025-07-15 03:30:52 +0330

    fix chaotic bug, add extensions

--- install.sh
+++ install.sh
@@ -3,19 +3,21 @@
 # Define all packages we need for this configuration
 packages=(
     # Better UI 
-    power-profiles-daemon gst-plugin-pipewire ttf-dejavu ttf-jetbrains-mono ttf-nerd-fonts-symbols la-capitaine-icon-theme
+    power-profiles-daemon gst-plugin-pipewire ttf-dejavu ttf-jetbrains-mono ttf-nerd-fonts-symbols la-capitaine-icon-theme gnome-browser-connector gnome-bluetooth
 )
 
 ################### Adding Chaotic AUR ###################
 
 # Getting Chaotic Aur key list
-# pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-# pacman-key --lsign-key 3056513887B78AEB
-# # Installing Chaotic AUR on computer
-# pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm --needed
-# pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm --needed
-# # Adding MirrorList to pacman.conf file 
-# cp ./data/pacman.txt /etc/pacman.conf
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+# Installing Chaotic AUR on computer
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm --needed
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm --needed
+# Adding MirrorList to pacman.conf file 
+cp ./data/pacman.txt /etc/pacman.conf
+# update pacman
+pacman -Syu
 
 
 ################### Configure neofetch ###################
@@ -60,3 +62,10 @@ gsettings set org.gnome.desktop.interface monospace-font-name "JetBrains Mono 11
 
 ################### Install Define Packages ###################
 pacman -S --noconfirm --needed "${packages[@]}"
+
+
+################### Extensions ###################
+cp -r ./extensions ~/.local/share/gnome-shell/extensions
+# enable 
+gnome-extensions enable appmenu-is-back@fthx
+gnome-extensions enable clipboard-indicator@tudmotu.com
